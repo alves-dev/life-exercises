b4434955daa4e2da9472d3e26ad5f1a36d0ca81d
igormoreiraalves <igor@biomainvestimentos.com>
2024-01-24 18:12:31 -0300

    CHORE: project init

---
+++ src/main/kotlin/com/alvesdev/life/exercises/ExercisesApplication.kt
@@ -0,0 +1,11 @@
+package com.alvesdev.life.exercises
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExercisesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExercisesApplication>(*args)
+}
---
+++ src/test/kotlin/com/alvesdev/life/exercises/ExercisesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alvesdev.life.exercises
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExercisesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
