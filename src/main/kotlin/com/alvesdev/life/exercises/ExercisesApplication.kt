2488b24ba7ddb5b4ad2b37cd1c4333db6edf4f67
igormoreiraalves <igor@biomainvestimentos.com>
2024-01-25 11:39:03 -0300

    CHORE: declare timezone

--- src/main/kotlin/com/alvesdev/life/exercises/ExercisesApplication.kt
+++ src/main/kotlin/com/alvesdev/life/exercises/ExercisesApplication.kt
@@ -1,11 +1,18 @@
 package com.alvesdev.life.exercises
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class ExercisesApplication
 
 fun main(args: Array<String>) {
 	runApplication<ExercisesApplication>(*args)
+
+	@PostConstruct
+	fun started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"))
+	}
 }
